2b7c2b8063bdd0b307fc0095627ad9484baf78be
Lachlan Brewster <lbr63@uclive.ac.nz>
2017-10-04 13:27:23 +1300

    added include comments for modules

--- game.c
+++ game.c
@@ -1,4 +1,13 @@
 #include "system.h"
+#include "pongBall.h"
+#include "pongPaddle.h"
+#include "led.h"
+#include "pio.h"
+#include "ledmat.h"
+#include "navswitch.h"
+#include "tinygl.h"
+#include "pacer.h"
+#include "pongGameStatus.h"
 
 int main (void)
 {
