45965ff7484dab25ff19fe4e07e0e327f44e3726
Gemma Lamont <gla42@uclive.ac.nz>
2017-10-11 12:17:35 +1300

    random

--- game.c
+++ game.c
@@ -14,12 +14,13 @@
 /** Welcome message to start game */
 void welcomeMsg (void)
 {
+    /**rnadom*/
     tinygl_init (1000);
     tinygl_font_set (&font3x5_1);
     tinygl_text_speed_set (5);
     tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
     tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
-    tinygl_text ("WELCOME TO PONG! ");
+    tinygl_text ("WELCOME TO PONG! ");ls
 }
 
 
